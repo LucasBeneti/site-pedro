aa5e9b0aeb9537366170204d574612ff2058972d
lbeneti <lucasbeneti@alunos.utfpr.edu.br>
2020-09-07 00:19:51 -0300

    Updates

---
+++ precache-manifest.4ab58c14f08bac2f089b9d5a08e8687c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b19da532966f5a7152fb3f3ab77de9",
+    "url": "/site-pedro/index.html"
+  },
+  {
+    "revision": "971f3dd4a44deaf0aa33",
+    "url": "/site-pedro/static/css/main.b954d8f4.chunk.css"
+  },
+  {
+    "revision": "48f2f268774329c4331b",
+    "url": "/site-pedro/static/js/2.08fb041f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site-pedro/static/js/2.08fb041f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971f3dd4a44deaf0aa33",
+    "url": "/site-pedro/static/js/main.7657c0fa.chunk.js"
+  },
+  {
+    "revision": "89ba1a7ec7383837b0c8",
+    "url": "/site-pedro/static/js/runtime-main.3cc6509e.js"
+  }
+]);
