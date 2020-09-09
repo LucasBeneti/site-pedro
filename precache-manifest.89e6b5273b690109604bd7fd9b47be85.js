dc8ac9baecedea5f8b92301e8435d7e6fe6f86c1
lbeneti <lucasbeneti@alunos.utfpr.edu.br>
2020-09-09 20:59:27 -0300

    Updates

---
+++ precache-manifest.89e6b5273b690109604bd7fd9b47be85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065d4c34bde9fdcf39267e48c0da5ed9",
+    "url": "/site-pedro/index.html"
+  },
+  {
+    "revision": "120a4ef7c127813b8330",
+    "url": "/site-pedro/static/css/main.b954d8f4.chunk.css"
+  },
+  {
+    "revision": "68dc7f1896d833468222",
+    "url": "/site-pedro/static/js/2.8e42f2b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site-pedro/static/js/2.8e42f2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120a4ef7c127813b8330",
+    "url": "/site-pedro/static/js/main.e2762be1.chunk.js"
+  },
+  {
+    "revision": "89ba1a7ec7383837b0c8",
+    "url": "/site-pedro/static/js/runtime-main.3cc6509e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b740a4a72196bbe0bcf0cf4f55e31c8.js"
+  "/site-pedro/precache-manifest.89e6b5273b690109604bd7fd9b47be85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site-pedro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
